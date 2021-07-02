360f3ee72ec760910f6026508ec1320ec61d92ba
tk <tk@renard.ninja>
2021-07-02 11:53:38 +0200

    ajoute une favicon

--- frontend/site/.vuepress/config.js
+++ frontend/site/.vuepress/config.js
@@ -23,7 +23,8 @@ module.exports = {
     [
       'meta',
       { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }
-    ]
+    ],
+    ['link', { rel: 'icon', href: '/assets/favicons/favicon.ico'}]
   ],
 
   /**
