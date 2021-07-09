0a8c1947bf8179ff669efc7c9f0d39da6f7e3efd
tk <tk@renard.ninja>
2021-07-09 14:43:11 +0200

    rajoute un bouton permettant de ré-initialiser les filtres
    closes #18

--- frontend/site/.vuepress/theme/global-components/InventoryFilter.vue
+++ frontend/site/.vuepress/theme/global-components/InventoryFilter.vue
@@ -1,51 +1,58 @@
 <template>
-  <div class="fr-grid-row fr-grid-row--gutters fr-mb-2w">
-    <div class="fr-col-2">
-      <label class="fr-label" for="select-type">
-        Type
-      </label>
-      <select v-model="type" class="fr-select" name="select-type" id="select-type" @mousedown="onSelect($event, type)">
-        <option value="" selected disabled hidden>Selectionnez un type</option>
-        <option v-for="type of types" :value="type.label">
-          {{ type.label }}
-        </option>
-      </select>
-    </div>
+  <div class="fr-mb-2w">
+    <div class="fr-grid-row fr-grid-row--gutters">
+      <div class="fr-col-2">
+        <label class="fr-label" for="select-type">
+          Type
+        </label>
+        <select v-model="type" class="fr-select" name="select-type" id="select-type" @mousedown="onSelect($event, type)">
+          <option value="" selected disabled hidden>Selectionnez un type</option>
+          <option v-for="type of types" :value="type.label">
+            {{ type.label }}
+          </option>
+        </select>
+      </div>
 
-    <div class="fr-col-3">
-      <label class="fr-label" for="select-status">
-        Statut d'ouverture
-      </label>
-      <select v-model="status" class="fr-select" name="select-status" id="select-status" @mousedown="onSelect($event, status)">
-        <option value="" selected disabled hidden>Selectionnez un statut</option>
-        <option v-for="status of statuses" :value="status.label">
-          {{ status.label }}
-        </option>
-      </select>
-    </div>
+      <div class="fr-col-3">
+        <label class="fr-label" for="select-status">
+          Statut d'ouverture
+        </label>
+        <select v-model="status" class="fr-select" name="select-status" id="select-status" @mousedown="onSelect($event, status)">
+          <option value="" selected disabled hidden>Selectionnez un statut</option>
+          <option v-for="status of statuses" :value="status.label">
+            {{ status.label }}
+          </option>
+        </select>
+      </div>
 
-    <div class="fr-col-3">
-      <label class="fr-label" for="select-trimester">
-        Trimestre
-      </label>
-      <select v-model="trimester" class="fr-select" name="select-trimester" id="select-trimester" @mousedown="onSelect($event, trimester)">
-        <option value="" selected disabled hidden>Selectionnez un trimestre</option>
-        <option v-for="t of trimesters" :value="t.key">
-          {{ t.label }}
-        </option>
-      </select>
-    </div>
+      <div class="fr-col-3">
+        <label class="fr-label" for="select-trimester">
+          Trimestre
+        </label>
+        <select v-model="trimester" class="fr-select" name="select-trimester" id="select-trimester" @mousedown="onSelect($event, trimester)">
+          <option value="" selected disabled hidden>Selectionnez un trimestre</option>
+          <option v-for="t of trimesters" :value="t.key">
+            {{ t.label }}
+          </option>
+        </select>
+      </div>
 
-    <div class="fr-col-4">
-      <label class="fr-label" for="select-org">
-        Ministère
-      </label>
-      <select v-model="org" class="fr-select" name="select-org" id="select-org" @mousedown="onSelect($event, org)">
-        <option value="" selected disabled hidden>Selectionnez une ministère</option>
-        <option v-for="org of organizations" :value="org.label">
-          {{ org.label }}
-        </option>
-      </select>
+      <div class="fr-col-4">
+        <label class="fr-label" for="select-org">
+          Ministère
+        </label>
+        <select v-model="org" class="fr-select" name="select-org" id="select-org" @mousedown="onSelect($event, org)">
+          <option value="" selected disabled hidden>Selectionnez une ministère</option>
+          <option v-for="org of organizations" :value="org.label">
+            {{ org.label }}
+          </option>
+        </select>
+      </div>
+    </div>
+    <div class="fr-grid-row fr-grid-row--right fr-text--sm reset">
+      <div class="fr-col-2">
+        <a @click="reset($event)" href="#">Ré-initialiser tous les filtres</a>
+      </div>
     </div>
   </div>
 </template>
@@ -80,7 +87,25 @@ export default {
         parent.value = "";
         parent.dispatchEvent(new Event("change"));
       }
+    },
+    reset (e) {
+      e.preventDefault();
+
+      const selects = this.$el.getElementsByTagName('select');
+      selects.forEach(select => {
+        select.value = "";
+        select.dispatchEvent(new Event("change"))
+      })
     }
   }
 }
 </script>
+
+<style scoped>
+.reset {
+  text-align: right;
+}
+.reset a {
+  color: #9C9C9C;
+}
+</style>
