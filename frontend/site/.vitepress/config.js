413220ac7014d490fc4137ac3393ba0ecdcee4cc
Nicolas KEMPF <nicolas@conciergerie.dev>
2023-08-07 13:10:50 +0200

    feat: :sparkles: add favicon

--- frontend/site/.vitepress/config.js
+++ frontend/site/.vitepress/config.js
@@ -11,6 +11,12 @@ export default defineConfigWithTheme({
   title: "Ouverture data.gouv.fr",
   description: "Tableau de suivi des ouvertures de données, codes sources et API publics",
   cleanUrls: true,
+  head: [
+    ['link', { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' }],
+    ['link', { rel: 'icon', href: '/favicon.svg', type: 'image/svg+xml' }],
+    ['link', { rel: 'shortcut icon', href: '/favicon.ico', type: 'image/x-icon' }],
+    ['link', { rel: 'manifest', href: '/manifest.webmanifest', crossorigin: 'use-credentials' }],
+  ],
   markdown: {
     externalLinks: { target: "" },
   },
