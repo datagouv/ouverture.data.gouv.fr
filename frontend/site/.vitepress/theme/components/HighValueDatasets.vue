2c9b39fd99dde18ffdabd3bf3d4863a1554be398
Thibaud Dauce <git@hadibut.fr>
2024-05-06 17:39:12 +0200

    Refactor into one component

--- frontend/site/.vitepress/theme/components/HighValueDatasets.vue
+++ frontend/site/.vitepress/theme/components/HighValueDatasets.vue
@@ -1,106 +1,39 @@
 <template>
-    <div>
-        <div class="fr-grid-row fr-grid-row--gutters fr-mb-2w">
-            <Select class="fr-col-12 fr-col-sm-6" name="select-category" placeholder="Tous les ensemble de données" label="Ensemble de données" :options="categories" v-model="filters.category"/>
-            <Select class="fr-col-12 fr-col-sm-6" name="select-theme" placeholder="Toutes les thématique" label="Thématique" :options="themes" v-model="filters.theme"/>
-            <Select class="fr-col-12 fr-col-sm-6" name="select-department" placeholder="Tous les ministères" label="Ministère de tutelle" :options="departments" v-model="filters.department"/>
-            <Select class="fr-col-12 fr-col-sm-6" name="select-producer" placeholder="Tous les producteurs" label="Producteur des données" :options="producers" v-model="filters.producer"/>
-            <Select class="fr-col-12 fr-col-sm-6" name="select-status" placeholder="Tous les statuts" label="Statut" :options="statuses" v-model="filters.status"/>
-        </div>
-        <div class="fr-table fr-table--no-caption fr-table fr-table--layout-fixed" v-show="loading == 'done'">
-            <table>
-                <caption>
-                    Ce tableau permet de suivre et de rendre compte de l’ouverture des jeux de données, algorithmes, codes sources et API publics.
-                </caption>
-                <thead>
-                <tr>
-                    <th>Titre</th>
-                    <th>Ensemble de données</th>
-                    <th>Thématique</th>
-                    <th>Ministère de tutelle</th>
-                    <th>Producteur</th>
-                    <th>Statut</th>
-                </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="dataset in filteredDatasets">
-                        <td><a :href="dataset['URL']">{{ dataset['TITRE'] }}</a></td>
-                        <td>{{ dataset['ENSEMBLE DE DONNÉES'] }}</td>
-                        <td>{{ dataset['THÉMATIQUE'] }}</td>
-                        <td>{{ dataset['MINISTÈRE DE TUTELLE'] }}</td>
-                        <td>{{ dataset['PRODUCTEUR'] }}</td>
-                        <td>
-                            <p class="fr-badge fr-badge--sm fr-badge--no-icon" :class="{
-                                'fr-badge--success': dataset['STATUT'] == 'Disponible sur data.gouv.fr',
-                                'fr-badge--info': dataset['STATUT'] == 'Disponible',
-                                'fr-badge--error': dataset['STATUT'] == 'Non disponible',
-                                'fr-badge--warning': dataset['STATUT'] == 'Planifié',
-                            }">{{ dataset['STATUT'] }}</p>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-        <em v-show="loading == 'failed'">Impossible de récupérer les données, veuillez réessayer ultérieurement.</em>
-        <em v-show="loading == 'loading'">Données en cours de chargement...</em>
-    </div>
+    <Table :filters="filters" endpoint="high_value_datasets">
+        <template #thead>
+            <th>Titre</th>
+            <th>Ensemble de données</th>
+            <th>Thématique</th>
+            <th>Ministère de tutelle</th>
+            <th>Producteur</th>
+            <th>Statut</th>
+        </template>
+
+        <template #row="{ line: dataset }">
+            <td><a :href="dataset['URL']">{{ dataset['TITRE'] }}</a></td>
+            <td>{{ dataset['ENSEMBLE DE DONNÉES'] }}</td>
+            <td>{{ dataset['THÉMATIQUE'] }}</td>
+            <td>{{ dataset['MINISTÈRE DE TUTELLE'] }}</td>
+            <td>{{ dataset['PRODUCTEUR'] }}</td>
+            <td>
+                <p class="fr-badge fr-badge--sm fr-badge--no-icon" :class="{
+                    'fr-badge--success': dataset['STATUT'] == 'Disponible sur data.gouv.fr',
+                    'fr-badge--info': dataset['STATUT'] == 'Disponible',
+                    'fr-badge--error': dataset['STATUT'] == 'Non disponible',
+                    'fr-badge--warning': dataset['STATUT'] == 'Planifié',
+                }">{{ dataset['STATUT'] }}</p>
+            </td>
+        </template>
+    </Table>
 </template>
 <script setup lang="ts">
-import { computed, onMounted, ref } from 'vue'
-import Select from './Select.vue'
-import { useData } from 'vitepress';
-import { useUrlSearchParams } from '@vueuse/core'
+import Table from './Table.vue'
 
-const filters = useUrlSearchParams('history', {
-    initialValue: {
-        category: null,
-        theme: null,
-        department: null,
-        producer: null,
-        status: null,
-    }
-})
-
-const loading = ref<'loading' | 'failed' | 'done'>('loading')
-const datasets = ref<Array<{
-    "TITRE": string;
-    "STATUT": string;
-    "URL": string;
-    "PRODUCTEUR": string;
-    "ENSEMBLE DE DONNÉES": string;
-    "MINISTÈRE DE TUTELLE": string;
-    "THÉMATIQUE": string;
-}>>([])
-
-const categories = computed(() => Array.from(new Set(datasets.value.map((dataset) => dataset['ENSEMBLE DE DONNÉES']))));
-const themes = computed(() => Array.from(new Set(datasets.value.map((dataset) => dataset['THÉMATIQUE']))));
-const departments = computed(() => Array.from(new Set(datasets.value.map((dataset) => dataset['MINISTÈRE DE TUTELLE']))));
-const producers = computed(() => Array.from(new Set(datasets.value.map((dataset) => dataset['PRODUCTEUR']))));
-const statuses = computed(() => Array.from(new Set(datasets.value.map((dataset) => dataset['STATUT']))));
-
-const filteredDatasets = computed(() => datasets.value.filter((dataset) => {
-    if (filters.category && filters.category !== dataset['ENSEMBLE DE DONNÉES']) return false
-    if (filters.theme && filters.theme !== dataset['THÉMATIQUE']) return false
-    if (filters.department && filters.department !== dataset['MINISTÈRE DE TUTELLE']) return false
-    if (filters.producer && filters.producer !== dataset['PRODUCTEUR']) return false
-    if (filters.status && filters.status !== dataset['STATUT']) return false
-
-    return true
-}))
-
-const load = async () => {
-    const { theme } = useData();
-    const { apiUrl } = theme.value;
-
-    try {
-        const response = await fetch(`${apiUrl}/high_value_datasets`)
-        datasets.value = await response.json()
-        loading.value = 'done'
-    } catch (e) {
-        console.error(e)
-        loading.value = 'failed'
-    }
-}
-
-onMounted(() => load())
+const filters = [
+    { slug: 'category', key_in_api: 'ENSEMBLE DE DONNÉES', placeholder: 'Tous les ensemble de données', label: 'Ensemble de données' },
+    { slug: 'theme', key_in_api: 'THÉMATIQUE', placeholder: 'Toutes les thématiques', label: 'Thématique' },
+    { slug: 'department', key_in_api: 'MINISTÈRE DE TUTELLE', placeholder: 'Tous les ministères', label: 'Ministère de tutelle' },
+    { slug: 'producer', key_in_api: 'PRODUCTEUR', placeholder: 'Tous les producteurs', label: 'Producteur des données' },
+    { slug: 'status', key_in_api: 'STATUT', placeholder: 'Tous les statuts', label: 'Statut' },
+]
 </script>
---
+++ frontend/site/.vitepress/theme/components/Table.vue
@@ -0,0 +1,80 @@
+<template>
+    <div>
+        <div class="fr-grid-row fr-grid-row--gutters fr-mb-2w">
+            <Select v-for="filter in filters" :key="filter.slug" class="fr-col-12 fr-col-sm-6" :name="`select-${filter.slug}`" :placeholder="filter.placeholder" :label="filter.label" :options="allValues[filter.slug].value" v-model="selectedFilters[filter.slug]"/>
+        </div>
+        <div class="fr-table fr-table--no-caption fr-table fr-table--layout-fixed" v-show="loading == 'done'">
+            <table>
+                <caption>
+                    Ce tableau permet de suivre et de rendre compte de l’ouverture des jeux de données, algorithmes, codes sources et API publics.
+                </caption>
+                <thead>
+                <tr>
+                    <slot name="thead" />
+                </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="line in filteredLines">
+                        <slot name="row" :line="line" />
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <em v-show="loading == 'failed'">Impossible de récupérer les données, veuillez réessayer ultérieurement.</em>
+        <em v-show="loading == 'loading'">Données en cours de chargement...</em>
+    </div>
+</template>
+<script setup lang="ts">
+import { computed, onMounted, ref } from 'vue'
+import Select from './Select.vue'
+import { useData } from 'vitepress';
+import { useUrlSearchParams } from '@vueuse/core'
+
+const props = defineProps<{
+    endpoint: 'high_value_datasets' | 'ministerial_commitments';
+    filters: Array<{
+        slug: string;
+        key_in_api: string;
+        label: string;
+        placeholder: string;
+    }>
+}>()
+
+const selectedFilters = useUrlSearchParams('history', {
+    initialValue: Object.fromEntries(props.filters.map((filter) => [filter.slug, null]))
+})
+
+const loading = ref<'loading' | 'failed' | 'done'>('loading')
+const lines = ref<Array<any>>([])
+
+const allValues = Object.fromEntries(
+    props.filters.map((filter) => [
+        filter.slug,
+        computed(() => Array.from(new Set(lines.value.map((line) => line[filter.key_in_api])))),
+    ])
+)
+
+const filteredLines = computed(() => lines.value.filter((line) => {
+    for (let filter of props.filters) {
+        if (selectedFilters[filter.slug] && selectedFilters[filter.slug] !== line[filter.key_in_api]) return false
+    }
+
+    return true
+}))
+
+const load = async () => {
+    const { theme } = useData();
+    const { apiUrl } = theme.value;
+
+    try {
+        const response = await fetch(`${apiUrl}/${props.endpoint}`)
+        lines.value = await response.json()
+        loading.value = 'done'
+    } catch (e) {
+        console.error(e)
+        loading.value = 'failed'
+    }
+}
+
+onMounted(() => load())
+</script>
